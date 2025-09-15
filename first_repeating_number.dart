6b5c359bdbb30b0e096679d14ff3758a8e939a48
Gomathi Sivaraj <141650448+GomathiSivara@users.noreply.github.com>
2025-09-15 12:28:22 +0530

    Add files via upload
    project updated

---
+++ first_repeating_number.dart
@@ -0,0 +1,5 @@
+import 'package:first_repeating_number/first_repeating_number.dart' as first_repeating_number;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_repeating_number.calculate()}!');
+}
